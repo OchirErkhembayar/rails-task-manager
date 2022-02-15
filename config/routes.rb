d92ee9d2d7449d0513abf42783fcddfa7e225fd8
Ochir Erkhembayar <ochir_erkhembayar@yahoo.com>
2022-02-15 13:35:56 +0000

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # show specific
   get 'tasks/:id', to: 'tasks#show', as: :task
   # update
-  get 'tasks/:id/edit', to: 'task#edit', as: :edit_task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
   # delete
   delete 'tasks/:id', to: 'tasks#destroy'
