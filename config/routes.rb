cbf7608ebbd5c7c52699f402c494a73baec5c005
Ochir Erkhembayar <ochir_erkhembayar@yahoo.com>
2022-02-15 12:17:14 +0000

    index done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # show all
+  get '/tasks', to: 'tasks#index'
+  # create
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  # show specific
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # update
+  get 'tasks/:id/edit', to: 'task#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  # delete
+  delete 'tasks/:id', to: 'tasks#destroy'
+  # rails generate model Restaurant name:string rating:integer
 end
